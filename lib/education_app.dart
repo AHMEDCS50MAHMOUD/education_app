3cd024bf1ace738ad730176fc6eb777026d316d4
Ahmed Darwish <ahmed.mahmoud.40rhh@gmail.com>
2026-01-25 22:31:02 +0200

    Initial clean commit

---
+++ lib/education_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class EducationApp extends StatelessWidget {
+  const EducationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      /* initialRoute: '/',
+       routes: {
+        //'/': (context) => Homescreen(),
+        '/Result': (context) => ResultScreen(),
+       },*/
+     //   home: SplashScreen()  //ResultScreen()
+    );
+
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:education_app/education_app.dart';
+
+void main() {
+  runApp(EducationApp());
+}
