5e235a371e410f9e1638a37e1d9d3e4895487799
AhmedYounis <AhmedYounis@DESKTOP-3FLO90K>
2026-01-26 20:31:23 +0400

    Add login Screen

--- lib/education_app.dart
+++ lib/education_app.dart
@@ -1,3 +1,4 @@
+import 'package:education_app/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,7 +13,7 @@ class EducationApp extends StatelessWidget {
         //'/': (context) => Homescreen(),
         '/Result': (context) => ResultScreen(),
        },*/
-     //   home: SplashScreen()  //ResultScreen()
+        home: LoginScreen()  //ResultScreen()
     );
 
   }
