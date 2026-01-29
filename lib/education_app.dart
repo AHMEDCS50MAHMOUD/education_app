149bb08878c2f59badd7194d6025f638ce95936d
Hazem-elsayed-nassar <Hazem.totos66@gmail.com>
2026-01-29 06:26:19 +0200

    splash #2

--- lib/education_app.dart
+++ lib/education_app.dart
@@ -1,9 +1,5 @@
-import 'package:education_app/Screens/courses_screen.dart';
-import 'package:education_app/Screens/login_screen.dart';
-import 'package:education_app/Screens/register_screen.dart';
+import 'package:education_app/Screens/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:education_app/screens/notifications_screen.dart';
-
 
 class EducationApp extends StatelessWidget {
   const EducationApp({super.key});
@@ -12,22 +8,8 @@ class EducationApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      /* initialRoute: '/',
-       routes: {
-        //'/': (context) => Homescreen(),
-        '/Result': (context) => ResultScreen(),
-       },*/
-
-     
-
-
-
-
-      
-       
-       // home: RegisterScreen()  //ResultScreen()
-
+      theme: ThemeData(fontFamily: 'GentiumBookPlus'),
+      home: SplashScreen(),
     );
-
   }
 }
